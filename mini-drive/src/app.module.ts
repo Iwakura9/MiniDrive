fe2af01b26ba3dd82aa057c3686dbc1474aec387
Sousa <trsh9sousa@gmail.com>
2026-09-09 23:05:41 -0300

    add auth and file modules

--- mini-drive/src/app.module.ts
+++ mini-drive/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [FilesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
