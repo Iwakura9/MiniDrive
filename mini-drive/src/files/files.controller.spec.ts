9c1be5baf5c04a019c5080985635bababdda9fa6
Sousa <trsh9sousa@gmail.com>
2026-09-16 18:44:56 -0300

    test: repair existing Nest test setup

--- mini-drive/src/files/files.controller.spec.ts
+++ mini-drive/src/files/files.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesController } from './files.controller';
+import { FilesService } from './files.service';
 
 describe('FilesController', () => {
   let controller: FilesController;
@@ -7,6 +8,18 @@ describe('FilesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilesController],
+      providers: [
+        {
+          provide: FilesService,
+          useValue: {
+            listFiles: jest.fn(),
+            saveFile: jest.fn(),
+            saveFiles: jest.fn(),
+            renameFile: jest.fn(),
+            deleteFile: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<FilesController>(FilesController);
